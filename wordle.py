53b9dafe232f84ab27fe08ef2bd7b1ab3fa5517b
Côme <44554692+comejv@users.noreply.github.com>
2022-02-26 23:03:34 +0100

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -10,19 +10,19 @@ def input_valide(secret: str, tour: int):
         secret (str): mot à deviner
 
     Returns:
-        list: liste des lettres de la porposition
+        tuple: mot sans accents et mot avec accents du dic
     """
-    prop = input(f"Proposition {tour}/6 :\n")
+    prop = unidecode(input(f"Proposition {tour}/6 :\n"))
 
-    while not(unidecode(prop) in dic_sans_accents) or len(prop) != len(secret):
-        if not(unidecode(prop) in dic_sans_accents):
+    while not(prop in dic_sans_accents) or len(prop) != len(secret):
+        if not(prop in dic_sans_accents):
             prop = input("Votre mot n'est pas dans notre dictionnaire, réessayez :\n")
         elif len(prop) < len(secret):
             prop = input("Mot trop court :\n")
         elif len(prop) > len(secret):
             prop = input("Mot trop long :\n")
     
-    return list(dic_accents[dic_sans_accents.index(prop)])
+    return (list(prop),list(dic_accents[dic_sans_accents.index(prop)]))
 
 def output(secret: list, prop: list):
     """Donne les indices pour chaque lettre
@@ -35,11 +35,11 @@ def output(secret: list, prop: list):
         output (list): rouge mauvaise lettre, bleu mauvais emplacement, vert bon emplacement
     """
     output = []
-    for l in prop:
-        if l in secret:
-            if prop.index(l) == secret.index(l):
-                output.append(colored('◉', 'green'))
-            else:
+    secret_sans_accent = [unidecode(l) for l in secret]
+    for i in range(len(secret)):
+        if unidecode(secret[i])==unidecode(prop[i]):
+            output.append(colored('◉', 'green'))
+        elif unidecode(prop[i]) in secret_sans_accent:
                 output.append(colored('◉', 'blue'))
         else:
             output.append(colored('◉', 'red'))
@@ -65,14 +65,14 @@ def main():
     for chance in range(1,7):
         prop_mot = input_valide(mot_secret, chance)
 
-        if prop_mot == mot_secret:
+        if prop_mot[0] == mot_secret:
             print(f"\n{colored('Bravo','green')} ! Vous avez deviné le mot ",
                   *[colored(l.upper(),'green',attrs=['bold']) for l in mot_secret_accents], ".")
             return input("\nPour rejouer entrez 'oui'.\n")
 
-        indices = output(mot_secret, prop_mot)
+        indices = output(mot_secret, prop_mot[0])
         
-        print(*[colored(l.upper(),attrs=['bold']) for l in prop_mot],sep=' ')
+        print(*[colored(l.upper(),attrs=['bold']) for l in prop_mot[1]],sep=' ')
         print(*indices, sep=' ')
     print(f"\n{colored('Dommage','red')}, vous avez épuisé votre nombre de chances.\nLe mot était ",
           *[colored(l.upper(),'red',attrs=['bold']) for l in mot_secret_accents])
